f6bb2d9cab09e74917cf11820f0c11be1d5aeea9
Samuel Lim <samuellimck97@gmail.com>
2018-01-04 14:31:46 +0800

    implement user create

--- index.php
+++ index.php
@@ -1,17 +1,24 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
-
+$base = "/QCBackend";
 
 $klein = new \Klein\Klein();
 
 
-$klein->respond(function(){
+$klein->respond($base,function(){
     print(json_encode([
 	'success' => 'false',
 	'msg' => 'Illegal request'
     ]));
 	return 'hello world';
 });
+
+$klein->respond("POST", $base . "/user/create", function($response){
+	echo "Hi how are you :)";
+	$address = $response->param("address");
+});
+
 $klein-> dispatch();
+
 
 ?>
